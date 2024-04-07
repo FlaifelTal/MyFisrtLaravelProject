05412ee4f3b4ff520f4334de8a2eb9032ebbb33d
Tala Flaifel <flaifeltala@gmail.com>
2024-04-07 16:17:43 +0200

    modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('home', function () {
+    return "<h1>Home</h1>";
+}) -> name('home');
+
+
+Route::get('about', function () {
+    return "<h1>About</h1>";
+}) -> name('anout');
+
+Route :: group(['prefix'=>'customer'],function(){// to not have customer in all of the routes we add prefix 
+    Route :: get('/',function(){ 
+        return "<h1>Customer list</h1>";
+    });
+    
+    
+    Route :: get('/create',function(){
+        return "<h1>Customer create</h1>";
+    });
+    
+    Route :: get('/show',function(){
+        return "<h1>Customer show</h1>";
+    });
+});
